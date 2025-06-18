9017f8e5f4d38e59b2e9d17e22cabaef76dc3659
Carlos Berilo De Souza <carlos.berilo.ext@ipiranga.ipiranga>
2025-06-18 18:26:41 -0300

    Criar entidades

---
+++ src/main/java/com/assembleia/adm/AdmAssembleiaApplication.java
@@ -0,0 +1,13 @@
+package com.assembleia.adm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmAssembleiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmAssembleiaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/assembleia/adm/core/domain/Cooperado.java
@@ -0,0 +1,15 @@
+package com.assembleia.adm.core.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cooperado")
+public class Cooperado {
+}
